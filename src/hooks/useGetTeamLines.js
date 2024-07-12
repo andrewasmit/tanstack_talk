bb7b7bf67fc7771ff8fe5e13c707c2dc74928bd8
andrew <andrewasmit@hotmail.com>
2024-07-12 06:40:29 -0500

    chore: add comments/documentation

--- src/hooks/useGetTeamLines.js
+++ src/hooks/useGetTeamLines.js
@@ -5,17 +5,18 @@ const proxy = "https://cors-anywhere-gzhu.onrender.com/"
 const dailyFaceOffAPIBaseURL = "https://www.dailyfaceoff.com/_next/data/uIiikkd2u32fY37bbYsYO/teams/"
 const dailyFaceOffAPITag ="/line-combinations.json"
 
-
-export const fetchTeamData = async (url) => {
+// This is the function I use to execute a simple GET request from a URL that is passed in
+const fetchTeamData = async (url) => {
   const res = await fetch(url);
   const data = await res.json();
 
   if (res.status === 200) {
-    return data.pageProps;
+    return data.pageProps; // the .pageProps is a specificity to this particular API. You would normally just return 'data'
   } else throw new Error(data.message);
 };
 
 
+// Custom hook to take in the string of a Team Name, build the url we need to make the GET request to, and then utilize TanStack query accordingly
 export const useGetTeamLines = (teamName) => {
   const url = useMemo(() => {
     return proxy + dailyFaceOffAPIBaseURL + teamName + dailyFaceOffAPITag;
