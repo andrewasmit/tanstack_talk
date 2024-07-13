09c24109fb3d14782afcdfe269e8e72af9132c81
andrew <andrewasmit@hotmail.com>
2024-07-12 21:07:24 -0500

    add very basic components to click team logos and use data fetch accordingly

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 // External Dependencies
-// import { useState, useEffect } from 'react'
+import { 
+  useState, 
+  // useEffect 
+} from 'react'
 import { useGetTeamLines } from './hooks/useGetTeamLines';
 
 // Internal Dependencies
+import { allTeams } from './allTeams';
+import { useCallback } from 'react';
 
 function App() {
   // const [teamData, setTeamData] = useState({});
+  const [currentTeam, setCurrentTeam] = useState('');
 
   // const proxy = "https://cors-anywhere-gzhu.onrender.com/"
   // const dailyFaceOffAPIBaseURL = "https://www.dailyfaceoff.com/_next/data/uIiikkd2u32fY37bbYsYO/teams/"
@@ -32,7 +38,7 @@ function App() {
 
 
   // USING TAN STACK QUERY
-  const { data: teamData } = useGetTeamLines('dallas-stars');
+  const { data: teamData } = useGetTeamLines(currentTeam);
   
   // const { 
     //   data: teamData,
@@ -43,9 +49,26 @@ function App() {
     
     console.log("TEAM DATA: ", teamData);
 
+    const handleClickTeam = useCallback((id) => {
+      return setCurrentTeam(id)
+    }, []);
+
+
+    const teamsToDisplay = allTeams.map( team => {
+      return <div 
+              id={team.id} 
+              key={team.id}
+              onClick={() => handleClickTeam(team.id)}
+            >
+        <img src={`https://www.dailyfaceoff.com${team.src}`} width={100}/>
+      </div>
+    })
+
   return (
     <>
-      Hello, World
+      <h1>Hockey Teams</h1>
+      <h3>Click to See Team's Player Info</h3>
+      {teamsToDisplay}
     </>
   )
 }
