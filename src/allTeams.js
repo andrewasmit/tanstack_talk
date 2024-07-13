882195b96e2a3eb6f99c3357199857d397453008
andrew <andrewasmit@hotmail.com>
2024-07-12 20:50:08 -0500

    feat: add all teams data with team logo and slug for data fetching

---
+++ src/allTeams.js
@@ -0,0 +1,130 @@
+export const allTeams = [
+  {
+    id: "anaheim-ducks",
+    src: "/_next/static/media/anaheim-ducks.ccb6c2b6.png",
+  },
+  {
+    id: "boston-bruins",
+    src: "/_next/static/media/boston-bruins.7c11d08d.png",
+  },
+  {
+    id: "buffalo-sabres",
+    src: "/_next/static/media/buffalo-sabres.5cc30cec.png",
+  },
+  {
+    id: 'calgary-flames',
+    src: "/_next/static/media/calgary-flames.f2f92abb.png",
+  },
+  {
+    id: 'carolina-hurricanes',
+    src: "/_next/static/media/carolina-hurricanes.b2eafb35.png",
+  },
+  {
+    id: 'chicago-blackhawks',
+    src: "/_next/static/media/chicago-blackhawks.ee37f442.png",
+  },
+  {
+    id: 'colorado-avalanche',
+    src: "/_next/static/media/colorado-avalanche.b7138c02.png",
+  },
+  {
+    id: 'columbus-blue-jackets',
+    src: "/_next/static/media/columbus-blue-jackets.6342c43e.png",
+  },
+  {
+    id: 'dallas-stars',
+    src: "/_next/static/media/dallas-stars.d5ec2749.png",
+  },
+  {
+    id: 'detroit-red-wings',
+    src: "/_next/static/media/detroit-red-wings.113890ed.png",
+  },
+  {
+    id: 'edmonton-oilers',
+    src: "/_next/static/media/edmonton-oilers.633226bf.png",
+  },
+  {
+    id: 'florida-panthers',
+      src: "/_next/static/media/florida-panthers.67f4950b.png",
+  },
+  {
+    id: 'los-angeles-kings',
+    src: "/_next/static/media/los-angeles-kings.22f66836.png",
+  },
+  {
+    id: 'minnesota-wild',
+    src: "/_next/static/media/minnesota-wild.72839331.png",
+  },
+  {
+    id: 'montreal-canadiens',
+    src: "/_next/static/media/montreal-canadiens.ca64a529.png",
+  },
+  {
+    id: 'nashville-predators',
+    src: "/_next/static/media/nashville-predators.76750da4.png",
+  },
+  {
+    id: 'new-jersey-devils',
+    src: "/_next/static/media/new-jersey-devils.efa88323.png",
+  },
+  {
+    id: 'new-york-islanders',
+    src: "/_next/static/media/new-york-islanders.e5861a8d.png",
+  },
+  {
+    id:'new-york-rangers',
+    src: "/_next/static/media/new-york-rangers.3ba5ca3f.png",
+  },
+  {
+    id: 'ottawa-senators',
+    src: "/_next/static/media/ottawa-senators.df9fa045.png",
+  },
+  {
+    id: 'philadelphia-flyers',
+    src: "/_next/static/media/philadelphia-flyers.b02d81e7.png",
+  },
+  {
+    id: 'pittsburgh-penguins',
+    src: "/_next/static/media/pittsburgh-penguins.e5b4fa3e.png",
+  },
+  {
+    id: 'san-jose-sharks',
+    src: "/_next/static/media/san-jose-sharks.fd532d67.png",
+  },
+  {
+    id: 'seattle-kraken',
+    src: "/_next/static/media/seattle-kraken.9d4491a7.png",
+  },
+  {
+    id: 'st-louis-blues',
+    src: "/_next/static/media/st-louis-blues.9809041a.png",
+  },
+  {
+    id:'tampa-bay-lightning',
+    src: "/_next/static/media/tampa-bay-lightning.c651a731.png",
+  },
+  {
+    id: 'toronto-maple-leafs',
+    src: "/_next/static/media/toronto-maple-leafs.8688d30a.png",
+  },
+  {
+    id:' utah-hockey-club',
+    src: "/_next/static/media/utah-hockey-club.03502a67.png",
+  },
+  {
+    id: 'vancouver-canucks',
+    src: "/_next/static/media/vancouver-canucks.e4b10e26.png",
+  },
+  {
+    id: 'vegas-golden-knights',
+    src: "/_next/static/media/vegas-golden-knights.0e0640fa.png",
+  },
+  {
+    id: 'washington-capitals',
+    src: "/_next/static/media/washington-capitals.832b005a.png",
+  },
+  {
+    id: 'winnipeg-jets',
+    src: "/_next/static/media/winnipeg-jets.61d1cf52.png",
+  }
+];
