4fdd86ddb2099ac7d0e22166078322cae35811b4
andrew <andrewasmit@hotmail.com>
2024-07-20 18:26:46 -0500

    refactor: add grid styles to modal

--- src/Linemates.jsx
+++ src/Linemates.jsx
@@ -28,6 +28,26 @@ function Linemates({ data, handleClose }) {
     return null
   }, [data]);
 
+
+  linesToDisplay.splice(0,0,<div className="grid-full grid-header">Forwards</div>);
+  linesToDisplay.splice(1,0,<div className="grid-third grid-subheader">LW</div>);
+  linesToDisplay.splice(2,0,<div className="grid-third grid-subheader">C</div>);
+  linesToDisplay.splice(3,0,<div className="grid-third grid-subheader">RW</div>);
+  linesToDisplay.splice(16,0,<div className="grid-full grid-header">Defensive Pairs</div>);
+  linesToDisplay.splice(17,0,<div className="grid-half grid-subheader">LD</div>);
+  linesToDisplay.splice(18,0,<div className="grid-half grid-subheader">RD</div>);
+  linesToDisplay.splice(25,0,<div className="grid-full grid-header">Goalies</div>);
+  linesToDisplay.splice(26,0,<div className="grid-half grid-subheader">Starting</div>);
+  linesToDisplay.splice(27,0,<div className="grid-half grid-subheader">Backup</div>);
+  linesToDisplay.splice(30,0,<div className="grid-full grid-header">Power Play 1</div>);
+  linesToDisplay.splice(36,0,<div className="grid-full grid-header">Power Play 2</div>);
+  linesToDisplay.splice(42,0,<div className="grid-full grid-header">Penalty Kill 1</div>);
+  linesToDisplay.splice(47,0,<div className="grid-full grid-header">Penalty Kill 2</div>);
+
+  if (linesToDisplay.length > 52){
+    linesToDisplay.splice(52,0,<div className="grid-full grid-header">Injured Reserve</div>);
+  }
+
   return (
     <div className="lines-container">
       <button className="close-modal" onClick={handleClose}>X</button>
