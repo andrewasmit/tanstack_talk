33b7651b9159891297955fa8126d99eb2cb258e2
andrew <andrewasmit@hotmail.com>
2024-07-20 19:05:51 -0500

    chore: restrucutre and fix typos

--- src/main.jsx
+++ src/main.jsx
@@ -9,10 +9,10 @@ import {
 // Internal Dependencies
 import App from './App.jsx'
 
-// Local Variabels
+// Local Variables
 // export const queryClient = new QueryClient(); 
-// The next line by itself is perfectly acceptable to get started, 
-// but these defaults can be altered to better fit your project.
+// The previous line by itself is perfectly acceptable to get started, 
+// but some simple default settings can be altered to better fit your project.
 // This link will break down what and how to make those adjustments as I use in the next example
 // https://tanstack.com/query/latest/docs/framework/react/guides/important-defaults 
 
@@ -22,12 +22,12 @@ export const queryClient = new QueryClient({
     queries: {
       cacheTime: 1000 * 60 * 60 * 24,
       staleTime: 1000 * 60 * 60 * 24,
-    },
+    }, // 1000 Milliseconds * 60 Seconds * 60 Minutes * 24 Hours === This formula keeps data "fresh" for an entire day
   },
-});
+}); 
+
 // The only difference between queryClient on line 18 as opposed to line 18 is that
 // I have adjusted the time that queries are considered stale and triggered to refetch
-// 1000 Milliseconds * 60 Seconds * 60 Minutes * 24 Hours === This formula keeps data "fresh" for an entire day
 // Again, see the above link to read more on this
 
 ReactDOM.createRoot(document.getElementById('root')).render(
