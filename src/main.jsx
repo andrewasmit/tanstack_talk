489ffc5d520fc14bcba8e5b3aeba7885fa0d46d9
andrew <andrewasmit@hotmail.com>
2024-07-26 06:13:51 -0500

    feat: add dev tools

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 // Internal Dependencies
 import App from './App.jsx'
@@ -35,6 +36,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
 )
