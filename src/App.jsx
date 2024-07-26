1bb3abba6f08439e34e33370c2bf1f9f9bf38436
andrew <andrewasmit@hotmail.com>
2024-07-26 06:09:17 -0500

    chore: update comment documentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // External Dependencies
 import { 
   useState, 
-  useEffect 
+  // useEffect 
 } from 'react'
 
 // Internal Dependencies
@@ -13,33 +13,33 @@ import LinesModal from './components/LinesModal';
 import './App.css'
 
 function App() {
-  const [currentTeam, setCurrentTeam] = useState('');
-  const [teamData, setTeamData] = useState();
+  const [currentTeam, setCurrentTeam] = useState(''); 
+  // const [teamData, setTeamData] = useState();
 
-  const proxy = "https://cors-anywhere-gzhu.onrender.com/"
-  const dailyFaceOffAPIBaseURL = "https://www.dailyfaceoff.com/_next/data/W9FRt5PFIRBjmn5x2i5ae/teams/"
-  const dailyFaceOffAPITag ="/line-combinations.json"
+  // const proxy = "https://cors-anywhere-gzhu.onrender.com/"
+  // const dailyFaceOffAPIBaseURL = "https://www.dailyfaceoff.com/_next/data/W9FRt5PFIRBjmn5x2i5ae/teams/"
+  // const dailyFaceOffAPITag ="/line-combinations.json"
 
-  const fetchTeamData = async (teamName) => {
-    const url = proxy + dailyFaceOffAPIBaseURL + teamName + dailyFaceOffAPITag;
+  // const fetchTeamData = async (teamName) => {
+  //   const url = proxy + dailyFaceOffAPIBaseURL + teamName + dailyFaceOffAPITag;
 
-    const res = await fetch(url);
-    const data = await res.json();
+  //   const res = await fetch(url);
+  //   const data = await res.json();
 
-    if (res.status === 200) {
-      return data;
-    } else throw new Error(data.message);
+  //   if (res.status === 200) {
+  //     return data;
+  //   } else throw new Error(data.message);
 
-  };
+  // };
 
-  useEffect(() => {   
-    fetchTeamData(currentTeam)
-    .then(res => {
-      setTeamData(res.pageProps);
-    });
-  }, [currentTeam]);
+  // useEffect(() => {   
+  //   fetchTeamData(currentTeam)
+  //   .then(res => {
+  //     setTeamData(res.pageProps);
+  //   });
+  // }, [currentTeam]);
 
-  console.log("TEAM DATA in App.js: ", teamData);
+  // console.log("TEAM DATA in App.js: ", teamData);
 
   const { 
     handleOpen: openPopup, 
@@ -63,7 +63,7 @@ function App() {
         <LinesModal 
           currentTeam={currentTeam}
           handleClose={closePopup}
-          teamData={teamData} // When using the TanStack hook, this will no longer be a prop as this data will be handled directly in the Linemates modal
+          // teamData={teamData} // When using the TanStack hook, this will no longer be a prop as this data will be handled directly in the Linemates modal
         />
       }
     </>
